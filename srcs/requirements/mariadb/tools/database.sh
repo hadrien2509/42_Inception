60e95aa34aea421df89c79441728c8f4561b91be
Hadrien Geissler <hadrien2509@gmail.com>
2023-09-19 10:44:46 +0200

    .

--- srcs/requirements/mariadb/tools/database.sh
+++ srcs/requirements/mariadb/tools/database.sh
@@ -1,26 +1,14 @@
-#!/bin/sh
+#!/bin/bash
 
-# Creates the directory used to store socket files and gives permissions to the mysql user
-if [ ! -d "/run/mysqld" ]; then
-	mkdir -p /run/mysqld
-	chown -R mysql:mysql /run/mysqld
-fi
+service mysql start 
 
-# Initializes database and gives permissions to mysql user
-if [ ! -d "/var/lib/mysql/mysql" ]; then
-	mysql_install_db --user=mysql --datadir=/var/lib/mysql > /dev/null 2>&1
-	chown -R mysql:mysql /var/lib/mysql
-fi
+mysql -e "CREATE DATABASE IF NOT EXISTS ${SQL_DATABASE};"
+mysql -e "CREATE USER IF NOT EXISTS ${SQL_USER}@'%' IDENTIFIED BY ${SQL_PASSWORD} ;"
+mysql -e "GRANT ALL PRIVILEGES ON ${SQL_DATABASE}.* TO '${SQL_USER}'@'%' WITH GRANT OPTION ;"
+mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${SQL_ROOT_PASSWORD}';"
+mysql -e "FLUSH PRIVILEGES ;"
 
-# Ensures that the server listens for network connections
-sed -i "s|skip-networking|# skip-networking|g" /etc/my.cnf.d/mariadb-server.cnf
+mysqladmin -u root -p $SQL_ROOT_PASSWORD shutdown
 
-# Creates a database with a user and changes the root password
-echo "CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE;" > db.sql
-echo "CREATE USER IF NOT EXISTS $MYSQL_USER@'%' IDENTIFIED BY '$MYSQL_PASSWORD';" >> db.sql
-echo "GRANT ALL PRIVILEGES ON $MYSQL_DATABASE.* TO $MYSQL_USER@'%';" >> db.sql
-echo "ALTER USER 'root'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD';" >> db.sql
-echo "FLUSH PRIVILEGES ;" >> db.sql
+exec mysqld_safe
 
-# Starts the server in daemon mode by executing the sql file
-mysqld --bind-address 0.0.0.0 --user=mysql --init-file=/home/db.sql
