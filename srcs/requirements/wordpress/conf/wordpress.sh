fcfb5e97f0a73a8547915aa81660ca26cd446b79
Tharlo <hadrien2509@gmail.com>
2023-10-03 13:02:30 +0200

    wordpress install check

--- srcs/requirements/wordpress/conf/wordpress.sh
+++ srcs/requirements/wordpress/conf/wordpress.sh
@@ -1,29 +1,34 @@
 #!/bin/bash
 
-cd /var/www/wordpress
-sleep 5
-echo "=> Creating a new configuration file..."
-wp config create \
-		--allow-root \
-		--dbname=$MYSQL_DATABASE \
-		--dbuser=$MYSQL_USER \
-		--dbpass=$MYSQL_PASSWORD \
-		--dbhost=$MYSQL_HOSTNAME
-echo "=> Configuration file created!"
-wp core install \
-		--allow-root \
-		--url=$DOMAIN_NAME \
-		--title=$WP_TITLE \
-		--admin_user=$WP_ROOT_USER \
-		--admin_password=$WP_ROOT_PASSWORD \
-		--admin_email=$WP_ROOT_EMAIL
+if [ -f ./wp-config-sample.php ]
+then
+	echo "Wordpress already installed!"
 
-wp user create "${WP_USER}" "${WP_USER_EMAIL}" \
-	--user_pass="${WP_USER_PASSWORD}" \
-	--role=author \
-	--allow-root
+else
+	cd /var/www/wordpress
+	sleep 5
+	echo "=> Creating a new configuration file..."
+	wp config create \
+			--allow-root \
+			--dbname=$MYSQL_DATABASE \
+			--dbuser=$MYSQL_USER \
+			--dbpass=$MYSQL_PASSWORD \
+			--dbhost=$MYSQL_HOSTNAME
+	echo "=> Configuration file created!"
+	wp core install \
+			--allow-root \
+			--url=$DOMAIN_NAME \
+			--title=$WP_TITLE \
+			--admin_user=$WP_ROOT_USER \
+			--admin_password=$WP_ROOT_PASSWORD \
+			--admin_email=$WP_ROOT_EMAIL
 
-echo "=> Wordpress installed!"
+	wp user create "${WP_USER}" "${WP_USER_EMAIL}" \
+		--user_pass="${WP_USER_PASSWORD}" \
+		--role=author \
+		--allow-root
 
+	echo "=> Wordpress installed!"
+fi
 
-/usr/sbin/php-fpm7.4 -F
+exec /usr/sbin/php-fpm7.4 -F
